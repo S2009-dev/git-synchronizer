e9b6977861e5fb5ef86fc81b317ba71e19bb2926
S2009 <contact@s2009.fr>
2025-03-16 18:06:22 +0100

    Add realease support

--- www/index.php
+++ www/index.php
@@ -10,6 +10,7 @@ $config = Yaml::parse($yamlContent);
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
 
+// Verify the github signature header with the secret key -> https://docs.github.com/en/webhooks-and-events/webhooks/securing-your-webhooks
 function verifySignature($secret, $header, $payload) {
     $parts = explode("=", $header);
     if (count($parts) !== 2 || $parts[0] !== 'sha256') {
@@ -32,21 +33,36 @@ function verifySignature($secret, $header, $payload) {
     }
 }
 
+// Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $signatureHeader = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
     $payload = file_get_contents('php://input');
     $secret = $_ENV["SECRET"];
     $base_url = $_ENV["BASE_URL"];
 
+    // Verify the signature
     if (verifySignature($secret, $signatureHeader, $payload)) {
         $data = json_decode($payload, true);
 
         if(isset($data['zen'])){
+            // Handle ping event
             http_response_code(200);
             echo "Ping received";
-        } elseif (isset($data['commits'])) {
+        } elseif (isset($data['commits']) || (isset($data['release']) && $data['action'] != "deleted" )) {
+            // Handle commit event
             $repo = $data['repository'];
             $conf = $config[$repo['owner']['name']][$repo['name']]['commits'];
+
+            if(isset($data['release'])){
+                $conf = $config[$repo['owner']['name']][$repo['name']]['releases'];
+            }
+
+            if(!isset($conf) || empty($conf)){
+                // Handle configuration not found
+                http_response_code(404);
+                echo "No configuration found for this repository";
+            }
+
             $source = $conf['source'];
             $dest = $conf['dest'];
             $cpdir = $conf['cpdir'];
@@ -63,21 +79,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             exec($command, $output, $return_var);
 
             if ($return_var !== 0) {
+                // Handle error
                 http_response_code(500);
                 echo 'An internal error occurred: ' . implode("\n", $output);
             } else {
+                // Handle success
                 http_response_code(200);
                 echo 'Success';
             }
         } else {
+            // Handle unknown event
             http_response_code(400);
             echo "Bad Request";
         }
     } else {
+        // Handle invalid signature
         http_response_code(401);
         echo "Invalid Signature";
     }
 } else {
+    // Handle invalid request method
     http_response_code(400);
     echo "Bad Request Method";
 }
